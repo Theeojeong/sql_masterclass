9a0205754c698498705731cdd3c56b88b8119300
Theeojeong <wogusto13@gmail.com>
2025-12-01 01:04:58 +0900

    251201 update

---
+++ PostgreSQL/#14 functions and procedures/#3 trigger functions.sql
@@ -0,0 +1,15 @@
+CREATE FUNCTION set_updated_at()
+RETURNS TRIGGER AS
+$$
+	BEGIN
+		NEW.updated_at = CURRENT_TIMESTAMP;
+		RETURN NEW;
+	END
+$$
+LANGUAGE plpgsql;
+
+
+CREATE TRIGGER updated_at
+BEFORE UPDATE
+ON movies
+FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
